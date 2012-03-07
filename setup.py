68399532622c6073f16e84e33d0a72f7f7ccec30
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2012-03-07 11:51:07 +0900

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='catalpa-aihun',
+    version='1.0',
+    license="",
+
+    install_requires = [
+"rapidsms",
+
+],
+
+    description="The common base package for catalpa's projects.",
+    long_description=open('README.md').read(),
+    author='Anders Hofstee, Nicoas Hoibian',
+    author_email='a.hofstee@catalpainternational.org',
+
+    url='https://github.com/catalpainternational/aihun',
+    include_package_data=True,
+
+    packages=['aihun'],
+    package_data={'aihun':['README.md']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
